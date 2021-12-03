0daae8c23a32c0ef95e1f8467b3e9678ba267e2d
yvonnezhao <32623146+yvonniefan@users.noreply.github.com>
2021-12-03 13:16:45 -0500

    Create parameters.R

---
+++ parameters.R
@@ -0,0 +1,37 @@
+
+# Disease transmission
+tao_1 = 6
+tao_2 = 10
+mu = 0.66
+alpha = 0.2 * y
+T = 14
+C_max = 13
+i_N = 0.037
+
+# Test related
+T_sp = 0.998
+T_sn = 0.8
+T_c = 500 # test/day
+k_s = 0.01
+k_E = 0.6
+
+# Population
+A = 1786 #person/day
+tao_arrival = 14
+a_s = 0.97
+A_E = 0.003
+a_IU = 0.0015
+B_3060 = 2500
+B_gt60 = 500
+
+# Policy
+M = 0
+theta = 1
+h = 100
+beta_3060 = 1
+beta_gt60 = 1
+
+# Fatality
+f_lt30 = 0.00004
+f_3060 = 0.0005
+f_gt60 = 0.03
