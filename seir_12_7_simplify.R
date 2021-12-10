bfc0cbe3e78d33f8f860ba51b5043a1680585d92
yvonnezhao <32623146+yvonniefan@users.noreply.github.com>
2021-12-10 13:27:01 -0500

    Update seir_12_7_simplify.R

--- seir_12_7_simplify.R
+++ seir_12_7_simplify.R
@@ -1,6 +1,5 @@
-
+library(tidyverse)
 # ------ Simulation function --------
-
 simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, C_max=13, i_N=0.037,
                     E0=10,I0=1,R0=0, len=200) {
   alpha=0.2*y
@@ -15,7 +14,6 @@ simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, 
     E = E0
     I = I0
     R = R0
-    
     # Transmission dynamics:
     # Code below are based on equation 3,4,6,8 from the article (simplified)
     S0 = max(0, S - i_N*C_max*S/N * (mu*E + alpha*I))  # equation 3
@@ -29,70 +27,27 @@ simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, 
     record[i,4] = R0
   }
   record$days = c(1:len)
-
-  colors <- c("Susceptible" = "black",
-              "Exposed" = "yellow", 
-              "Infected" = "red", 
-              "Recovered" = "green")
-
+  
   return(record)
 }
 
+# Get daily record: you should use simfunc() as data when calling this function:
+func_getdaily <- function(data){
+  daily <- data %>% select(c(1:5)) %>% 
+    mutate(lags=lag(susceptible), lage=lag(exposed),lagi=lag(infected),lagr=lag(recovered),
+           d_sus = pmax(0,lags-susceptible, na.rm=TRUE),
+           d_exp = pmax(0,exposed-lage, na.rm=TRUE), 
+           d_inf = pmax(0,infected-lagi, na.rm=TRUE), 
+           d_rec = pmax(0,recovered-lagr, na.rm=TRUE)) %>%
+    select(days, d_sus, d_exp,d_inf,d_rec)
+  return(daily)
+}
+
+# ourrecord <- simfunc()
+# daily <- func_getdaily(ourrecord)
+
 # -------- Shiny App ---------
 library(plotly)
-# 
-# tao_1 = 6 # day; average time to move from early to late stage of the disease
-# tao_2 = 10 # day; infection post-exposure period
-# mu = 0.66 # relative infectivity of exposed to undocumented cases
-# y=1 # scaler
-# alpha = 0.2 * y # relative infectivity of documented to undocumented cases
-# # T = 14
-# C_max = 13 # constant contact rate; person/day
-# i_N = 0.037 # infection probablity absense of mask; person/day
-# 
-# # Test related - not used yet
-# T_sp = 0.998
-# T_sn = 0.8
-# T_c = 500 # test/day
-# k_s = 0.01
-# k_E = 0.6
-# 
-# # Population
-# A = 400 # person/day
-# tao_arrival = 14 # arrival duration
-# a_s = 0.97 
-# A_E = 0.003
-# a_IU = 0.0015
-# B_3060 = 2500
-# B_gt60 = 500
-
-# Policy - not used yet
-# M = 0
-# theta = 1
-# h = 100
-# beta_3060 = 1
-# beta_gt60 = 1
-# 
-# # Fatality - not used yet
-# f_lt30 = 0.00004
-# f_3060 = 0.0005
-# f_gt60 = 0.03
-
-# # ---- initialization -----
-# S0_inc = A*tao_arrival
-# N = A*tao_arrival # initial population
-# E0 = 10 # initial exposure
-# I0 = 1 # initial infection
-# R0 = 0 # initial recovered
-len = 200 # days of observation
-# record = data.frame(matrix(0,nrow=len, ncol=4))
-# 
-# # rates:
-# alpha = 0.0002 * y # relative infectivity of documented to undocumented cases
-# mu = 0.66 # relative infectivity of exposed to undocumented cases
-# r_recover = 1/tao_2
-
-
 
 ui <- fluidPage(
   
@@ -121,7 +76,8 @@ ui <- fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotlyOutput("distPlot")
+      plotlyOutput("distPlot"),
+      plotlyOutput("distPlotdaily")
     )
   )
 )
@@ -130,16 +86,18 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   record_reac <- reactive({ 
-    simfunc(A = input$students,C_max = input$cmax, i_N =input$i_n )
+    simfunc(A = input$students,C_max = input$cmax, i_N =input$i_n)
     })
-  
+  daily_reac <- reactive({ 
+    # record <- simfunc(A = input$students,C_max = input$cmax, i_N =input$i_n)
+    func_getdaily(record_reac())
+  })
   # plot
   
   colors <- c("Susceptible" = "black",
               "Exposed" = "yellow", 
               "Infected" = "red", 
               "Recovered" = "green")
-  # days <- 1:len
   
   output$distPlot <- renderPlotly({
     ggplot(data=record_reac()) +
@@ -151,7 +109,16 @@ server <- function(input, output) {
       scale_color_manual(values = colors)
   })
   
-  
+  # New: plot that shows daily cases
+  output$distPlotdaily <- renderPlotly({
+    ggplot(data=daily_reac()) +
+        geom_line(aes(x=days, y=d_sus, color="Susceptible"))+
+        geom_line(aes(x=days, y=d_exp, color="Exposed"))+
+        geom_line(aes(x=days, y=d_inf, color="Infected"))+
+        geom_line(aes(x=days, y=d_rec, color="Recovered")) +
+      labs(y="Daily cases", x= "Days")+
+      scale_color_manual(values = colors)
+  })
 }
 
 # Run the application 
