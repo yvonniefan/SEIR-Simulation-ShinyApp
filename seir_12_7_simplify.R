070dbb04ad9a3b3ff291b48e6c767e9b7a2f4c9f
yvonnezhao <32623146+yvonniefan@users.noreply.github.com>
2021-12-11 22:27:17 -0500

    update cumulative plot and some simulation code
    Dec 10

--- seir_12_7_simplify.R
+++ seir_12_7_simplify.R
@@ -1,13 +1,24 @@
 library(tidyverse)
 # ------ Simulation function --------
 simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, C_max=13, i_N=0.037,
-                    E0=10,I0=1,R0=0, len=200) {
-  alpha=0.2*y
-  r_recover=1/tao_2
-  S0 = A*tao_arrival
-  N = A*tao_arrival
+                    E0=10,I0=1,R0=0, len=120) {
+  alpha = 0.2 * y # relative infectivity of documented to undocumented cases
+  r_recover=1/tao_2 # recover rate
+  S0 = A*tao_arrival # initial Susceptible
+  N = A*tao_arrival  # initial population, same as initial susceptible
   record = data.frame(matrix(0,nrow=len, ncol=4))
   colnames(record) <- c('susceptible', 'exposed', 'infected','recovered')
+  
+  # new:
+  phi = 1 #1 or 0, university closure decision, 0 open, 1 closed
+  T = 14 # days; average time to evacuate the college town after school closure
+  
+  L_S = phi*S0/T
+  L_E = phi*E0/T
+  L_I = phi*I0/T
+  L_R = phi*R0/T
+  A_S = 0.97*A #daily arrival rate of students with status susceptible
+  A_E = 0.003*A
   
   for (i in 1:len){
     S = S0
@@ -17,9 +28,13 @@ simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, 
     # Transmission dynamics:
     # Code below are based on equation 3,4,6,8 from the article (simplified)
     S0 = max(0, S - i_N*C_max*S/N * (mu*E + alpha*I))  # equation 3
-    E0 = max(0, E + i_N*C_max*S/N * (mu*E + alpha*I) - E/tao_1) # equation 4
-    I0 = max(0, I + E/tao_1 - I0/tao_2) # equation 6
+    E0 = max(0, E + i_N*C_max*S/N * (mu*E + alpha*I) - E/tao_1)  # equation 4
+    I0 = max(0, I + E/tao_1 - I0/tao_2 )# equation 6
     R0 = max(0, R + I0/tao_2) # equation 8
+    # S0 = max(0, S - i_N*C_max*S/N * (mu*E + alpha*I) +A_S - L_S)  # equation 3
+    # E0 = max(0, E + i_N*C_max*S/N * (mu*E + alpha*I) +A_E - E/tao_1   - L_E)  # equation 4
+    # I0 = max(0, I + E/tao_1 - I0/tao_2  -  L_I)# equation 6
+    # R0 = max(0, R + I0/tao_2  - L_R) # equation 8
     
     record[i,1] = S0
     record[i,2] = E0
@@ -31,17 +46,15 @@ simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, 
   return(record)
 }
 
-# Get daily record: you should use simfunc() as data when calling this function:
-func_getdaily <- function(data){
-  daily <- data %>% select(c(1:5)) %>% 
-    mutate(lags=lag(susceptible), lage=lag(exposed),lagi=lag(infected),lagr=lag(recovered),
-           d_sus = pmax(0,lags-susceptible, na.rm=TRUE),
-           d_exp = pmax(0,exposed-lage, na.rm=TRUE), 
-           d_inf = pmax(0,infected-lagi, na.rm=TRUE), 
-           d_rec = pmax(0,recovered-lagr, na.rm=TRUE)) %>%
-    select(days, d_sus, d_exp,d_inf,d_rec)
-  return(daily)
-}
+
+
+
+# Get cumulative record: you should use simfunc() as data when calling this function:
+func_getcum <- function(data){
+  cum <- data %>% mutate(cum_E = cumsum(exposed),
+                                cum_I = cumsum(infected), 
+                                cum_R = cumsum(recovered))
+  return(cum)
 
 # ourrecord <- simfunc()
 # daily <- func_getdaily(ourrecord)
@@ -88,9 +101,9 @@ server <- function(input, output) {
   record_reac <- reactive({ 
     simfunc(A = input$students,C_max = input$cmax, i_N =input$i_n)
     })
-  daily_reac <- reactive({ 
+  cum_reac <- reactive({ 
     # record <- simfunc(A = input$students,C_max = input$cmax, i_N =input$i_n)
-    func_getdaily(record_reac())
+    func_getcum(record_reac())
   })
   # plot
   
@@ -105,19 +118,28 @@ server <- function(input, output) {
       geom_line(aes(x=days, y=exposed, color="Exposed"))+
       geom_line(aes(x=days, y=infected, color="Infected"))+
       geom_line(aes(x=days, y=recovered, color="Recovered"))+
-      labs(y="Cumulative cases", x= "Days")+
+      labs(y="Daily Cases", x= "Days")+
       scale_color_manual(values = colors)
   })
   
   # New: plot that shows daily cases
   output$distPlotdaily <- renderPlotly({
-    ggplot(data=daily_reac()) +
-        geom_line(aes(x=days, y=d_sus, color="Susceptible"))+
-        geom_line(aes(x=days, y=d_exp, color="Exposed"))+
-        geom_line(aes(x=days, y=d_inf, color="Infected"))+
-        geom_line(aes(x=days, y=d_rec, color="Recovered")) +
-      labs(y="Daily cases", x= "Days")+
-      scale_color_manual(values = colors)
+    
+    ggplot(data=cum_reac()) +
+      geom_line(aes(x=days, y=cum_E, color="Exposed"))+
+      geom_line(aes(x=days, y=cum_I, color="Infected"))+
+      # geom_line(aes(x=days, y=cum_R, color="Recovered")) +
+      labs(y="Cumulative Cases", x= "Days")+
+      scale_color_manual(values = c( "Exposed" = "yellow", 
+                                     "Infected" = "red"))
+    
+    # ggplot(data=daily_reac()) +
+    #     geom_line(aes(x=days, y=d_sus, color="Susceptible"))+
+    #     geom_line(aes(x=days, y=d_exp, color="Exposed"))+
+    #     geom_line(aes(x=days, y=d_inf, color="Infected"))+
+    #     geom_line(aes(x=days, y=d_rec, color="Recovered")) +
+    #   labs(y="Daily cases", x= "Days")+
+    #   scale_color_manual(values = colors)
   })
 }
 
