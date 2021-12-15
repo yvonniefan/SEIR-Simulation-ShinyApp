b89aa2f367ee247d9a2f8ae1c22966402ac944fd
Yvonne <yvonniefan@gmail.com>
2021-12-15 17:43:27 -0500

    update simulation and app

--- seir12_13_prod.R
+++ seir12_13_prod.R
@@ -47,76 +47,7 @@ corfaculty= 1688
 cornell <- corstudents + corfaculty
 # ------ simulation ---------
 # source('~/Downloads/PHP2560R/simulation_YZ.R')
-
-# ------ Simulation function --------
-
-simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, C_max=13, i_N=0.037,
-                    E0=10,I0=1,R0=0, len=120, phi = 1, h = 100) {
-  alpha = 0.2 * y # relative infectivity of documented to undocumented cases
-  r_recover=1/tao_2 # recover rate
-  S0 = A*tao_arrival # initial Susceptible
-  N = A*tao_arrival  # initial population, same as initial susceptible
-  # h = 100 # coefficient representing the sensitivity of a college to the number of daily cases
-  
-  record = data.frame(matrix(0,nrow=len, ncol=4))
-  colnames(record) <- c('susceptible', 'exposed', 'infected','recovered')
-  record[1,'C'] = C_max
-  
-  # new:
-  # phi = 1 #1 or 0, university closure decision, 0 open, 1 closed
-  T = 14 # days; average time to evacuate the college town after school closure
-  
-  L_S = phi*S0/T
-  L_E = phi*E0/T
-  L_I = phi*I0/T
-  L_R = phi*R0/T
-  A_S = 0.97*A #daily arrival rate of students with status susceptible
-  A_E = 0.003*A
-  
-  for (i in 1:len){
-    S = S0
-    E = E0
-    I = I0
-    R = R0
-    # Transmission dynamics:
-    # Code below are based on equation 3,4,6,8 from the article (simplified)
-    # S0 = max(0, S - i_N*C_max*S/N * (mu*E + alpha*I))  # equation 3
-    # E0 = max(0, E + i_N*C_max*S/N * (mu*E + alpha*I) - E/tao_1)  # equation 4
-    # I0 = max(0, I + E/tao_1 - I0/tao_2 )# equation 6
-    # R0 = max(0, R + I0/tao_2) # equation 8
-    S0 = max(0, S - i_N*record[max(i-1, 1), 'C'] *S/N * (mu*E + alpha*I) + A_S - L_S)  # equation 3
-    E0 = max(0, E + i_N*record[max(i-1, 1), 'C'] *S/N * (mu*E + alpha*I) + A_E - E/tao_1 - L_E)  # equation 4
-    I0 = max(0, I + E/tao_1 - I0/tao_2 - L_I)# equation 6
-    R0 = max(0, R + I0/tao_2 - L_R) # equation 8
-    
-    record[i,1] = S0
-    record[i,2] = E0
-    record[i,3] = I0
-    record[i,4] = R0
-    # 7 days rolling average:
-    record[i,'rollavg'] = mean(record[max((i-14),1) :i,3], na.rm=TRUE)
-    
-    
-    # W_N: factor that affect contact rate. Students tend to change behavior according to current covid cases
-    record[i, 'W_N'] = exp(-h * ((record[i,'rollavg'])/N))
-    record[i, 'C'] = C_max * record[i,'W_N']
-    # record[i, 'C'] = C_max 
-    
-  }
-  record$days = c(1:len)
-  # record$rollavg1 <- roll_mean(record$infected, n=7, align='right',fill=NA)
-  
-  return(record)
-}
-
-
-# Get cumulative record: you should use simfunc() as data when calling this function:
-func_getcum <- function(data){
-  cum <- data %>% mutate(cum_E = cumsum(exposed),
-                         cum_I = cumsum(infected),
-                         cum_R = cumsum(recovered))
-  return(cum)
-}
+source("simulation_part.R")
 
 # -------- Shiny App ---------
 library(plotly)
@@ -167,13 +98,19 @@ ui <- fluidPage(
       actionButton("variables", "Information about Inputs"),
       #new: bulleted list https://stackoverflow.com/questions/22923784/how-to-add-bullet-points-in-r-shinys-rendertext/22930719
       uiOutput("myList")
+      
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(type="tabs",
-                  tabPanel("Daily cases", plotlyOutput("distPlot"), textOutput('contact'), textOutput('text')),
-                  tabPanel("Cumulative cases", plotlyOutput("distPlotcum")),
+                  tabPanel("Daily cases", plotlyOutput("distPlot"), textOutput('text'), textOutput('contact')),
+                  tabPanel("Cumulative cases", plotlyOutput("distPlotcum"), textOutput('text2'), textOutput('contact2')),
+                  tabPanel("SEIR Curve", plotlyOutput("seir_curve"), textOutput('textseir')),
+                  tabPanel("Behaviour and Policy Analysis", plotOutput("analysis"),
+                          radioButtons("graph", "Choose Statistic:", choices = c("Daily Average", "Daily Maximum")),
+                          textOutput('textanalysis')),
+                  
                   tabPanel("Summary", verbatimTextOutput("summary"), 
                            p("C is contact rate. This is related to 
                              student sensitivity to COVID-19 and rolling average of COVID-19 cases."))),
@@ -256,16 +193,22 @@ server <- function(input, output) {
               "Exposed" = "yellow", 
               "Infected" = "red", 
               "Recovered" = "green")
+  colors2 <- c("Exposed" = "yellow", 
+               "Infected" = "red")
+  linetypes <- c('C'=3)
   
   #daily cases plot
   output$distPlot <- renderPlotly({
     ggplot(data=record_reac()) +
-      geom_line(aes(x=days, y=exposed, color="Exposed"))+
-      geom_line(aes(x=days, y=infected, color="Infected"))+
-      # New: plot contact rate changes over time:
-      geom_line(aes(x=days, y=C), linetype = 3) +
-      labs(y="Daily cases", x= "Days")+
-      scale_color_manual(values = colors)+
+      # geom_line(aes(x=days, y=susceptible, color="Susceptible")) +
+      geom_line(aes(x=days, y=exposed, color="Exposed")) +
+      geom_line(aes(x=days, y=infected, color="Infected")) +
+      geom_line(aes(x=days, y=C), linetype=3) +
+      # geom_line(aes(x=days, y=C), linetype=3) +
+      # geom_line(aes(x=days, y=rollavg), linetype=2) +
+      labs(y="Daily cases", x= "Days") +
+      scale_color_manual(name='Status', values = colors2) +
+      # annotate("text", x = 90, y = 20, label = 'Dotted line: Contact Rate', hjust = 0, size = 2) +
       ggtitle("Daily cases of COVID-19")
   })
   
@@ -275,26 +218,86 @@ server <- function(input, output) {
       geom_line(aes(x=days, y=cum_E, color="Exposed"))+
       geom_line(aes(x=days, y=cum_I, color="Infected"))+
       labs(y="Cumulative Cases", x= "Days")+
-      scale_color_manual(values = c( "Exposed" = "yellow", 
-                                     "Infected" = "red"))+
+      scale_color_manual(name='Status', values = c( "Exposed" = "yellow", 
+                                                    "Infected" = "red"))+
       ggtitle("Cumulative cases of COVID-19")
   })
+  # plot that shows all SEIR curves
+  output$seir_curve <- renderPlotly({
+    ggplot(data=cum_reac()) +
+      geom_line(aes(x=days, y=susceptible, color="Susceptible")) +
+      geom_line(aes(x=days, y=exposed, color="Exposed")) +
+      geom_line(aes(x=days, y=infected, color="Infected")) +
+      geom_line(aes(x=days, y=recovered, color="Recovered"))+
+      labs(y="Daily Number", x= "Days") +
+      scale_color_manual(values = colors) +
+      ggtitle("SEIR Model")
+  })
+  
   output$summary <- renderPrint({
     summary(simfunc())
   })
-  # 
+
   output$contact <- renderText({
+    paste0('Minimum contact rate: ', round(min(record_reac()$C),1),
+           '  Max contact rate: ', round(max(record_reac()$C),1), 
+           '  Average contact rate: ', round(mean(record_reac()$C),1))
+  })
+  output$contact2 <- renderText({
     paste0('Minimum contact rate: ', round(min(record_reac()$C),1), 
-           '  Max contact rate: ', max(record_reac()$C), 
+           '  Max contact rate: ', round(max(record_reac()$C),1), 
            '  Average contact rate: ', round(mean(record_reac()$C),1))
   })
   
-  #NEW: add description of plot 
+  # Add description of plots  
   output$text <- renderText({
-    "Yvonne - put description of plot (explain what black dotted line is perhaps?)
-    and maybe also explain the statistics above --> Get rid of this text when you 
-    put your stuff here!"
+    "Dotted line shows contact rate change over time, which depends on students sensitivity to daily COVID cases. Solid lines represent daily cases with status exposed and infected"
   })
+  output$text2 <- renderText({
+    "Dotted line shows contact rate change over time, which depends on students sensitivity to daily COVID cases. Solid lines represent daily cases with status exposed and infected"
+  })
+  output$textseir <- renderText({
+    "SEIR curve under the parameters chosen. Note when sensitivity is set to 0, the graph looks like a typical SEIR curve."
+  })
+  output$textanalysis <- renderText({
+    "Daily cases is negatively related to student sensitivity. In addition, when university conducts closure policy (green line), we would observe lower cases in both infected and exposed"
+  })
+  
+  # Behaviour ana Policy analysis:
+  sensi <- seq(1,20)*50 # initilize a list of sensitivities
+  policy1 <- analysis_behaviour(1, sensi)
+  policy1$policy = 1
+  policy0 <- analysis_behaviour(0, sensi)
+  policy0$policy = 0
+  policy2 <- rbind(policy0,policy1)
+  
+  plotmean <- reactive({
+    policy2 %>% filter(stat=='mean') %>%
+      ggplot() +
+      geom_line(aes(x=sensi, y=value, color=closure, linetype=status)) +
+      labs(x='Student Sensitivity', y='Daily Values') +
+      ggtitle('Daily Average by Student Sensitivity and University Closure Decision')
+  })
+  plotmax <- reactive({
+    policy2 %>% filter(stat=='max') %>%
+      ggplot() +
+      geom_line(aes(x=sensi, y=value, color=closure, linetype=status)) + 
+      labs(x='Student Sensitivity', y='Daily Values') +
+      ggtitle('Daily Maximum by Student Sensitivity and University Closure Decision')
+  })
+  
+  graphInput <- reactive({
+    switch(input$graph,
+           "Daily Average" = plotmean(),
+           "Daily Maximum" = plotmax()
+    )
+  })
+  output$analysis <- renderPlot({ 
+    graphInput()
+  })
+  
+  
+
   }
 
 
--- simulation_part.R
+++ simulation_part.R
@@ -1,24 +1,40 @@
 # This is the simulation/parameter code:
+
 library(tidyverse)
-require(RcppRoll)
-# install.packages('RcppRoll')
+library(docstring)
 # ------ Simulation function --------
 
 simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, C_max=13, i_N=0.037,
                     E0=10,I0=1,R0=0, len=120, phi = 1, h = 100) {
+  #' SEIR Simulation 
+  #' 
+  #' Generate SEIR simulation according to specified parameters
+  #' @param A numeric, rate of incoming students, default 1786
+  #' @param tao_arrival numeric, Arrival duration, default 14
+  #' @param tao_1 numeric,  exposure period: average time to move from early to late stage of the disease, default 6
+  #' @param tao_2 numeric,  infection post-exposure period, default 10
+  #' @param mu numeric,  relative infectivity of exposed to undocumented cases, default 0.66
+  #' @param  y numeric,  scaler, default 1
+  #' @param C_max numeric, constant contact rate absent any exogenous or endogenous changes in social behaviour, default 13
+  #' @param i_N numeric, infection probablity in the absense of mask, default 0.037
+  #' @param E0 numeric, initial exposure, default 10
+  #' @param I0 numeric, initial infected, default 1
+  #' @param R0 numeric, initial recovered, default 0
+  #' @param len numeric, Number of days simulated, default 120
+  #' @param phi numeric, 1 or 0, university closure decision, 0 open, 1 closed, default 1
+  #' @param h numeric, coefficient representing the sensitivity of a college to the number of daily cases; h=0 means students don't care at all about COVID transmission and keep at maximum contact rate, default 100
+  #' @return A dataframe that stores: daily number of cases of S, E, I, R status, 14-day rolling average of infected, contact rates, multiplier for changing contact rates
+  
+  # Initializations:
   alpha = 0.2 * y # relative infectivity of documented to undocumented cases
   r_recover=1/tao_2 # recover rate
   S0 = A*tao_arrival # initial Susceptible
   N = A*tao_arrival  # initial population, same as initial susceptible
-  # h = 100 # coefficient representing the sensitivity of a college to the number of daily cases
+  T = 14 # days; average time to evacuate the college town after school closure
   
   record = data.frame(matrix(0,nrow=len, ncol=4))
   colnames(record) <- c('susceptible', 'exposed', 'infected','recovered')
   record[1,'C'] = C_max
-  
-  # new:
-  # phi = 1 #1 or 0, university closure decision, 0 open, 1 closed
-  T = 14 # days; average time to evacuate the college town after school closure
   
   L_S = phi*S0/T
   L_E = phi*E0/T
@@ -33,14 +49,9 @@ simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, 
     I = I0
     R = R0
     # Transmission dynamics:
-    # Code below are based on equation 3,4,6,8 from the article (simplified)
-    # S0 = max(0, S - i_N*C_max*S/N * (mu*E + alpha*I))  # equation 3
-    # E0 = max(0, E + i_N*C_max*S/N * (mu*E + alpha*I) - E/tao_1)  # equation 4
-    # I0 = max(0, I + E/tao_1 - I0/tao_2 )# equation 6
-    # R0 = max(0, R + I0/tao_2) # equation 8
-    S0 = max(0, S - i_N*record[max(i-1, 1), 'C'] *S/N * (mu*E + alpha*I) + A_S - L_S)  # equation 3
-    E0 = max(0, E + i_N*record[max(i-1, 1), 'C'] *S/N * (mu*E + alpha*I) + A_E - E/tao_1 - L_E)  # equation 4
-    I0 = max(0, I + E/tao_1 - I0/tao_2 - L_I)# equation 6
+    S0 = max(0, S - i_N*record[max(i-1, 1), 'C'] *S/N * (mu*E + alpha*I) + A_S - L_S) # equation 3
+    E0 = max(0, E + i_N*record[max(i-1, 1), 'C'] *S/N * (mu*E + alpha*I) + A_E - E/tao_1 - L_E) # equation 4
+    I0 = max(0, I + E/tao_1 - I0/tao_2 - L_I) # equation 6
     R0 = max(0, R + I0/tao_2 - L_R) # equation 8
     
     record[i,1] = S0
@@ -49,33 +60,35 @@ simfunc <- function(A = 1786, tao_arrival = 14, tao_1=6, tao_2=10,mu=0.66, y=1, 
     record[i,4] = R0
     # 7 days rolling average:
     record[i,'rollavg'] = mean(record[max((i-14),1) :i,3], na.rm=TRUE)
-    
-    
     # W_N: factor that affect contact rate. Students tend to change behavior according to current covid cases
     record[i, 'W_N'] = exp(-h * ((record[i,'rollavg'])/N))
     record[i, 'C'] = C_max * record[i,'W_N']
-    # record[i, 'C'] = C_max 
-    
   }
   record$days = c(1:len)
-  # record$rollavg1 <- roll_mean(record$infected, n=7, align='right',fill=NA)
-  
   return(record)
 }
 
-ourrecord[2:9,3]
-sum(ourrecord[2:9,3])
+# docstring(simfunc)
 
 # Get cumulative record: you should use simfunc() as data when calling this function:
 func_getcum <- function(data){
+  #' Generate Cumulative Record
+  #' 
+  #' Generate a cumulative record table based on daily cases table
+  #' @param data numeric, dataframe that records daily cases of S, E, I, R status
+  #' @return A dataframe that calculates cumulative number of exposed, infected and recovered cases
   cumulative <- data %>% mutate(cum_E = cumsum(exposed),
                                 cum_I = cumsum(infected), 
                                 cum_R = cumsum(recovered))
   return(cumulative)
 }
+# docstring(func_getcum)
 
+
+# Below are just tests for generating graphs. The actual graphs in the ShinyApp are created in the ShinyApp server.
 ourrecord <- simfunc(A=100)
 cumulative <- func_getcum(ourrecord)
+library(plotly)
 
 
 colors <- c("Susceptible" = "black",
@@ -85,21 +98,22 @@ colors <- c("Susceptible" = "black",
 
 # daily cases plot:
 ggplot(data=ourrecord) +
-  # geom_line(aes(x=days, y=susceptible, color="Susceptible")) +
   geom_line(aes(x=days, y=exposed, color="Exposed")) +
   geom_line(aes(x=days, y=infected, color="Infected")) +
-  geom_line(aes(x=days, y=C), linetype=3) +
-  # geom_line(aes(x=days, y=recovered, color="Recovered"))+
+  geom_line(aes(x=days, y=C, linetype='C')) +
+  geom_line(aes(x=days, y=rollavg, linetype='roll')) +
   labs(y="Daily cases", x= "Days") +
-  scale_linetype_manual(name='contact rate' ,values="C")+
-  scale_color_manual(values = colors) 
-
+  scale_color_manual(name='Status',values = c( Exposed = "yellow", Infected = "red")) +
+  scale_linetype_manual(name='Contact Rate',values=c(C = 3, roll=2)) + 
+  annotate("text", x = 90, y = 20, label = 'Dotted line: Contact Rate', hjust = 0, size = 2) +
+  # annotation_custom('text', xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf)+
+  
+  ggtitle("Daily cases of COVID-19")
 
 # cumulative cases plot:
 ggplot(data=cumulative) +
   geom_line(aes(x=days, y=cum_E, color="Exposed"))+
   geom_line(aes(x=days, y=cum_I, color="Infected"))+
-  # geom_line(aes(x=days, y=cum_R, color="Recovered")) +
   labs(y="Cumulative Cases", x= "Days")+
   scale_color_manual(values = c( "Exposed" = "yellow",
                                  "Infected" = "red"))
@@ -110,13 +124,45 @@ ggplot(data=ourrecord)+
 
 
 # SEIR plot:
-ourrecord <- simfunc(A=100, h=0, phi=1)
 ggplot(data=ourrecord) +
   geom_line(aes(x=days, y=susceptible, color="Susceptible")) +
   geom_line(aes(x=days, y=exposed, color="Exposed")) +
   geom_line(aes(x=days, y=infected, color="Infected")) +
-  # geom_line(aes(x=days, y=C), linetype=3) +
   geom_line(aes(x=days, y=recovered, color="Recovered"))+
   labs(y="Daily cases", x= "Days") +
   scale_color_manual(values = colors)
+
+# Explore relationship between student sensitivity, university closure policy and infected:
+analysis_behaviour <- function(closure, sensitivity){
+  #' Analyze behaviour and COVID cases
+  #' 
+  #' Analyze relationship between student sensitivity, university closure policy and COIVD cases
+  #' @param closure numeric, university closre policy, 1 means university closed, 0 means university open
+  #' @param sensitivity numeric vector, students' sensitivity to daily COIVD cases
+  #' @return A dataframe that records statistics (mean, maximum, and standard deviation) of daily cases under different policy and students' sensitivities
+  stats = data.frame(matrix(nrow = length(sensi), ncol=6))
+  for (i in 1:length(sensi)){
+    runsim <- simfunc(phi=closure, h=sensitivity[i])
+    stats[i,1] <- mean(runsim$infected)
+    stats[i,2] <- max(runsim$infected)
+    stats[i,3] <- sd(runsim$infected)
+    stats[i,4] <- mean(runsim$exposed)
+    stats[i,5] <- max(runsim$exposed)
+    stats[i,6] <- sd(runsim$exposed)
+  }
+  colnames(stats) = c('meaninfect','maxinfect','sdinfect','meanexpose','maxexpose','sdexpose')
+  stats$sensi <- sensitivity
+  stats$closure <- as.factor(closure)
+  stats <- stats %>% 
+    pivot_longer(cols=c('meaninfect','maxinfect','sdinfect','meanexpose','maxexpose','sdexpose'), 
+                 names_to='statistic',
+                 values_to='value') %>%
+    mutate(status = substr(statistic,nchar(statistic) - 6+1, nchar(statistic)),
+           stat = substr(statistic, 1, nchar(statistic) -6) )
+  return(stats)
+}
+
+docstring(analysis_behaviour)
+sensi <- seq(1,20)*50 # initilize a list of sensitivities
+
 
